cf4287e7dbac54daf0e20796e75561eda388a93e
Luminol <5054241040@student.its.ac.id>
2024-10-28 12:19:07 +0700

    Delete Fundamental_Programming/Tugas/Tugas_Function directory

--- Fundamental_Programming/Tugas/Tugas_Function/Odd_number.py
+++
@@ -1,3 +0,0 @@
-def odd(num):
-    return len([i for i in num if i % 2 != 0])
-print(odd(num = list(map(int, input('Enter numbers : ').split()))))
--- Fundamental_Programming/Tugas/Tugas_Function/Sisa_apel.py
+++
@@ -1,3 +0,0 @@
-def apple_left(people, apple):
-    return apple % people
-print(apple_left(people = int(input('People : ')), apple = int(input('Apples : '))))
